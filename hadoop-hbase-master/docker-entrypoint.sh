f59ff70ebaec8ee116c2a663d7aeb78304a7bcfc
Hasan Can Saral <hasancansaral@gmail.com>
2016-06-08 10:23:14 +0300

    Start Hadoop and HBase automatically when container restarts

---
+++ hadoop-hbase-master/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "start-ssh-serf.sh"
+~/start-ssh-serf.sh
+
+echo "configure-members.sh"
+~/configure-members.sh
+
+echo "start-hadoop.sh"
+~/start-hadoop.sh
+
+echo "start-hbase.sh"
+~/start-hbase.sh
--- start-container.sh
+++ start-container.sh
@@ -13,7 +13,7 @@ fi
 # delete old master container and start new master container
 sudo docker rm -f master.krejcmat.com &> /dev/null
 echo "start master container..."
-sudo docker run -d -t --dns 127.0.0.1 -P --name master.krejcmat.com -h master.krejcmat.com -w /root krejcmat/hadoop-hbase-master:$tag&> /dev/null
+sudo docker run -d -t --restart=always --dns 127.0.0.1 -P --name master.krejcmat.com -h master.krejcmat.com -w /root krejcmat/hadoop-hbase-master:$tag&> /dev/null
 
 # get the IP address of master container
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master.krejcmat.com)
@@ -24,7 +24,7 @@ while [ $i -lt $N ]
 do
 	sudo docker rm -f slave$i.krejcmat.com &> /dev/null
 	echo "start slave$i container..."
-	sudo docker run -d -t --dns 127.0.0.1 -P --name slave$i.krejcmat.com -h slave$i.krejcmat.com -e JOIN_IP=$FIRST_IP krejcmat/hadoop-hbase-slave:$tag &> /dev/null
+	sudo docker run -d -t --restart=always --dns 127.0.0.1 -P --name slave$i.krejcmat.com -h slave$i.krejcmat.com -e JOIN_IP=$FIRST_IP krejcmat/hadoop-hbase-slave:$tag &> /dev/null
 	((i++))
 done
 
